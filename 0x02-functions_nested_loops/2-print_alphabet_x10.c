7380ea8c3c7fca93495b8a6252c5c0aef977dc15
Lain0x0 <lainyamato03@gmail.com>
2023-08-15 12:02:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,7 @@ void print_alphabet_x10(void)
 {
 	char ch;
 
-	int i;
-
-	i = 0;
-
-while (i < 10)
-{
-
-ch = 'a';
-while (ch <= 'z')
+for (ch = 'a' ; ch <= 'z' ; ch++)
 
 {
 _putchar(ch);
@@ -23,8 +15,5 @@ ch++;
 }
 
 	putchar('\n');
-i++;
-}
-
 }
 
