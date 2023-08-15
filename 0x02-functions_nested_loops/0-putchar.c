9f5498a58911d108516743eae31d63b596c94847
Lain0x0 <lainyamato03@gmail.com>
2023-08-15 09:32:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
+ *   main - entry point of our program
  *
- *  @c: The character to print
+ *  Description : A C program that print _putchar
  *
- *  Return: On success 1.
- *
- *  On error, -1 is returned, and errno is set appropriately
+ * Return: 0
 */
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  main - entry point of our program
+ *
+ *  Description : This is a A C program that print using _putchar function
+ *
+ *  Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+while (i > 0);
+{
+
+int _putchar(char alpha);
+i++;
+
+}
+
+return (0);
+}
