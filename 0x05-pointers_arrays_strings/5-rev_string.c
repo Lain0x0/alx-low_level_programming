5722d627cde2ea0d957e60e42029609e5fe9992c
Lain0x0 <lainyamato03@gmail.com>
2023-08-21 12:22:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s : string parameter input
+ *
+ * Return: Nothing
+ *
+ */
+
+void rev_string(char *s)
+{
+
+	int l, i;
+
+	char temp;
+
+
+	for (1 = 0; s[l] != '\n'; ++l)
+
+
+	for (i = 0; i < 1 / 2; i++)
+		{
+		temp = s[i];
+
+		s[i] = s[l - 1 - i];
+
+		s[l - 1 - i] = temp;
+		}
+
+}
+
