14d7013a20b273296a10b349ec6e047f703447f6
Lain0x0 <lainyamato03@gmail.com>
2023-09-14 05:48:18 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/function_pointers.h
+++ 0x10-variadic_functions/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
-#include <stddef.h>
-#include <stdlib.h>
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
