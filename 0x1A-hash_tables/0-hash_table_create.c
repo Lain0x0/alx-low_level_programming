292aa1de229c19cea3e1282f29e699ffadfe4159
Lain0x0 <lainyamato03@gmail.com>
2024-02-16 19:53:50 +0100

    0x0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i;
 
-	table = malloc(sizeof(hash_table));
+	if (size == 0)
+		return (NULL);
 
+	table = calloc(1, sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	 table->size = size;
-	 table->array = malloc(size, *sizeof(hash_node_t *));
-
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
 	return (table);
 }
