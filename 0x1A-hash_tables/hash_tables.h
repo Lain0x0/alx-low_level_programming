4dd5d82940f9ca3d950c042dd3e262132dc317c4
Lain0x0 <lainyamato03@gmail.com>
2024-02-16 19:30:45 +0100

    0x0

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,20 +34,6 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
-/**
- * struct hash_table - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- *         Each cell of this array is a pointer to the first node of a linked list,
- *         because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
 /* Functions */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
