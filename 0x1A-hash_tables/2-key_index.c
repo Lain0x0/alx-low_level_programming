f39d9cb1914cb9b8cfd393c561609af804189bd2
Lain0x0 <lainyamato03@gmail.com>
2024-02-16 20:56:19 +0100

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * k_index - returns the index of a key
+ * key_index - returns the index of a key
  * @key: key
  * @size: size of the array of the hash table
  * Return: index
  */
-unsigned long int k_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int i = hash_djb2(key) % size;
 
